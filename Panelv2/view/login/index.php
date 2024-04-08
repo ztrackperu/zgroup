950a7781cc4ba4e537899d561b851c913352b595
ztrackperu <ztrack@zgroup.com.pe>
2024-04-08 15:48:15 -0500

    inicio

--- Panelv2/view/login/index.php
+++ Panelv2/view/login/index.php
@@ -31,7 +31,7 @@ document.oncontextmenu=inhabilitar
         <div id="loginbox" style="margin-top:20px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
             <div class="panel panel-primary" >
                     <div class="panel-heading">
-                        <div class="panel-title">Bienvenidos al Sistema Intranet</div>
+                        <div class="panel-title">Bienvenidos al Sistema Intranet z2</div>
                         <div style="float:right; font-size: 80%; position: relative;  top:-10px"><a href="#my_modal" data-toggle="modal" >Olvido su contraseña?</a></div>
                     </div>     
 
